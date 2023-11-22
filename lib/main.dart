3442b425f64c605c3cabd8bc576f64502ce121f3
wadpas <wadpas@gmail.com>
2023-11-22 14:04:37 +0200

    added AuthState model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +30,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
+Future<void> _handleSignIn() async {
+  try {
+    await _googleSignIn.signIn();
+  } catch (error) {
+    print('hello');
+  }
+}
+
+void ff() async {
+  final LoginResult result = await FacebookAuth.instance
+      .login(); // by default we request the email and the public profile
+// or FacebookAuth.i.login()
+  if (result.status == LoginStatus.success) {
+    // you are logged
+    final AccessToken accessToken = result.accessToken!;
+  } else {
+    print(result.status);
+    print(result.message);
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -38,7 +68,20 @@ class HomePage extends StatelessWidget {
         title: const Text('Extagram'),
       ),
       body: Center(
-        child: Text('Extagram'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: _handleSignIn,
+              child: Text('sign'),
+            ),
+            Text('Extagram'),
+            ElevatedButton(
+              onPressed: ff,
+              child: Text('f'),
+            ),
+            Text('Extagram'),
+          ],
+        ),
       ),
     );
   }
