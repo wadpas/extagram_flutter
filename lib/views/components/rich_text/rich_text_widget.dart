7438a468aa0334fb271820057e55fad7214260e5
wadpas <wadpas@gmail.com>
2024-01-08 18:51:01 +0200

    added RichTextWidget

---
+++ lib/views/components/rich_text/rich_text_widget.dart
@@ -0,0 +1,39 @@
+import 'package:extagram_flutter/views/components/rich_text/link_text.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
+import 'package:extagram_flutter/views/components/rich_text/base_text.dart';
+
+class RichTextWidget extends StatelessWidget {
+  final Iterable<BaseText> texts;
+  final TextStyle? styleForAll;
+
+  const RichTextWidget({
+    super.key,
+    required this.texts,
+    this.styleForAll,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: texts.map(
+          (baseText) {
+            if (baseText is LinkText) {
+              return TextSpan(
+                  text: baseText.text,
+                  style: styleForAll?.merge(baseText.style),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = baseText.onTapped);
+            } else {
+              return TextSpan(
+                text: baseText.text,
+                style: styleForAll?.merge(baseText.style),
+              );
+            }
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
